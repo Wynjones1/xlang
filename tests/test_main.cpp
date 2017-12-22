55bb01d755d4200875fa112bdfe5c616620d489c
Stuart Jones <stuart@wynjones1.com>
2017-12-22 11:40:14 +0000

    n

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,9 @@
-#include <boost/test/unit_test.cpp>
+#define BOOST_TEST_MODULE test module name
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( lexer_test )
+// entry point:
+int main(int argc, char* argv[], char* envp[])
 {
+    return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
 }
